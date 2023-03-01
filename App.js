04063168b65db5770d6a0b9c6c3c86e7aadcdf96
Sheetal <126230789+sheetalAily@users.noreply.github.com>
2023-03-01 19:58:58 +0530

    js

---
+++ App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React from 'react'
+
+
+const App=()=> {
+  return (
+    <div>hello i am sheetal</div>
+  );
+}
+
+export default App;
