e73e71ebf940f598045c146670a7d734277b6a8d
Sheetal <126230789+sheetalAily@users.noreply.github.com>
2023-03-09 17:41:09 +0530

    JSON

---
+++ JSON/App.jsx
@@ -0,0 +1,50 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React from 'react'
+// import './global.css'
+// import './navbar.module.css'
+import JSON from './userdata.json'
+import Student from './Student';
+
+
+// import Nav1 from './components/Nav1';
+// import Post from './classBasedComponents/Post';
+// import Body from './classBasedComponents/Body';
+// import Chats from './classBasedComponents/Chats';
+// import Footer from './classBasedComponents/Footer';
+// import Header from './classBasedComponents/Header';
+// import Like from './classBasedComponents/Like';
+// import Profile from './classBasedComponents/Profile';
+// import Reels from './classBasedComponents/Reels';
+// import Status from './classBasedComponents/Status';
+// import Tag from './classBasedComponents/Tag';
+// import Camera from './function_based_components/Camera';
+// import CloseFriends from './function_based_components/CloseFriends';
+// import Delete from './function_based_components/Delete';
+// import Dp from './function_based_components/Dp';
+// import Filters from './function_based_components/Filters';
+// import Follow from './function_based_components/Follow';
+// import ProfileEdit from './function_based_components/ProfileEdit';
+// import Request from './function_based_components/Request';
+// import Share from './function_based_components/Share';
+// import Stories from './function_based_components/Stories';
+// import Logo from './components/Logo';
+// import Menu from './components/Menu';
+// import NavBars from './NavBar/NavBars';
+// import NavBars from './NavBar/NavBars';
+// import App from './App';
+// import Netflix from './NavBar/Netflix';
+
+
+
+const App=()=>
+{
+  return(
+    
+  <>
+    <Student payload={JSON}/>
+    </>
+    
+  )
+}
+export default App
---
+++ JSON/Student.jsx
@@ -0,0 +1,192 @@
+import React from 'react'
+
+
+const Student = (props) => {
+  return (
+    <div className='parentBlock'>
+        <div className='mainBlock'>
+            <img src={props.payload[0].studentPhoto} alt="" />
+            <h1>{props.payload[0].studentID}</h1>
+            <h2>{props.payload[0].student}</h2>
+            <h3>{props.payload[0].stream}</h3>
+            <h4>{props.payload[0].mockRating}</h4>
+            <h5>{props.payload[0].course}</h5>
+            <h6>{props.payload[0].studentPercent}</h6>
+        </div>
+        <div className='mainBlock'>
+            <img src={props.payload[1].studentPhoto} alt="" />
+            <h1>{props.payload[1].studentID}</h1>
+            <h2>{props.payload[1].student}</h2>
+            <h3>{props.payload[1].stream}</h3>
+            <h4>{props.payload[1].mockRating}</h4>
+            <h5>{props.payload[1].course}</h5>
+            <h6>{props.payload[1].studentPercent}</h6>
+        </div>
+        <div className='mainBlock'>
+            <img src={props.payload[2].studentPhoto} alt="" />
+            <h1>{props.payload[2].studentID}</h1>
+            <h2>{props.payload[2].student}</h2>
+            <h3>{props.payload[2].stream}</h3>
+            <h4>{props.payload[2].mockRating}</h4>
+            <h5>{props.payload[2].course}</h5>
+            <h6>{props.payload[2].studentPercent}</h6>
+        </div>
+        <div className='mainBlock'>
+            <img src={props.payload[3].studentPhoto} alt="" />
+            <h1>{props.payload[3].studentID}</h1>
+            <h2>{props.payload[3].student}</h2>
+            <h3>{props.payload[3].stream}</h3>
+            <h4>{props.payload[3].mockRating}</h4>
+            <h5>{props.payload[3].course}</h5>
+            <h6>{props.payload[3].studentPercent}</h6>
+        </div>
+        <div className='mainBlock'>
+            <img src={props.payload[4].studentPhoto} alt="" />
+            <h1>{props.payload[4].studentID}</h1>
+            <h2>{props.payload[4].student}</h2>
+            <h3>{props.payload[4].stream}</h3>
+            <h4>{props.payload[4].mockRating}</h4>
+            <h5>{props.payload[4].course}</h5>
+            <h6>{props.payload[4].studentPercent}</h6>
+        </div>
+        <div className='mainBlock'>
+            <img src={props.payload[5].studentPhoto} alt="" />
+            <h1>{props.payload[5].studentID}</h1>
+            <h2>{props.payload[5].student}</h2>
+            <h3>{props.payload[5].stream}</h3>
+            <h4>{props.payload[5].mockRating}</h4>
+            <h5>{props.payload[5].course}</h5>
+            <h6>{props.payload[5].studentPercent}</h6>
+        </div>
+        <div className='mainBlock'>
+            <img src={props.payload[6].studentPhoto} alt="" />
+            <h1>{props.payload[6].studentID}</h1>
+            <h2>{props.payload[6].student}</h2>
+            <h3>{props.payload[6].stream}</h3>
+            <h4>{props.payload[6].mockRating}</h4>
+            <h5>{props.payload[6].course}</h5>
+            <h6>{props.payload[6].studentPercent}</h6>
+        </div>
+        <div className='mainBlock'>
+            <img src={props.payload[7].studentPhoto} alt="" />
+            <h1>{props.payload[7].studentID}</h1>
+            <h2>{props.payload[7].student}</h2>
+            <h3>{props.payload[7].stream}</h3>
+            <h4>{props.payload[7].mockRating}</h4>
+            <h5>{props.payload[7].course}</h5>
+            <h6>{props.payload[7].studentPercent}</h6>
+        </div>
+        <div className='mainBlock'>
+            <img src={props.payload[8].studentPhoto} alt="" />
+            <h1>{props.payload[8].studentID}</h1>
+            <h2>{props.payload[8].student}</h2>
+            <h3>{props.payload[8].stream}</h3>
+            <h4>{props.payload[8].mockRating}</h4>
+            <h5>{props.payload[8].course}</h5>
+            <h6>{props.payload[8].studentPercent}</h6>
+        </div>
+        <div className='mainBlock'>
+            <img src={props.payload[9].studentPhoto} alt="" />
+            <h1>{props.payload[9].studentID}</h1>
+            <h2>{props.payload[9].student}</h2>
+            <h3>{props.payload[9].stream}</h3>
+            <h4>{props.payload[9].mockRating}</h4>
+            <h5>{props.payload[9].course}</h5>
+            <h6>{props.payload[9].studentPercent}</h6>
+        </div>
+        <div className='mainBlock'>
+            <img src={props.payload[10].studentPhoto} alt="" />
+            <h1>{props.payload[10].studentID}</h1>
+            <h2>{props.payload[10].student}</h2>
+            <h3>{props.payload[10].stream}</h3>
+            <h4>{props.payload[10].mockRating}</h4>
+            <h5>{props.payload[10].course}</h5>
+            <h6>{props.payload[10].studentPercent}</h6>
+        </div>
+        <div className='mainBlock'>
+            <img src={props.payload[11].studentPhoto} alt="" />
+            <h1>{props.payload[11].studentID}</h1>
+            <h2>{props.payload[11].student}</h2>
+            <h3>{props.payload[11].stream}</h3>
+            <h4>{props.payload[11].mockRating}</h4>
+            <h5>{props.payload[11].course}</h5>
+            <h6>{props.payload[11].studentPercent}</h6>
+        </div>
+        <div className='mainBlock'>
+            <img src={props.payload[12].studentPhoto} alt="" />
+            <h1>{props.payload[12].studentID}</h1>
+            <h2>{props.payload[12].student}</h2>
+            <h3>{props.payload[12].stream}</h3>
+            <h4>{props.payload[12].mockRating}</h4>
+            <h5>{props.payload[12].course}</h5>
+            <h6>{props.payload[12].studentPercent}</h6>
+        </div>
+        <div className='mainBlock'>
+            <img src={props.payload[13].studentPhoto} alt="" />
+            <h1>{props.payload[13].studentID}</h1>
+            <h2>{props.payload[13].student}</h2>
+            <h3>{props.payload[13].stream}</h3>
+            <h4>{props.payload[13].mockRating}</h4>
+            <h5>{props.payload[13].course}</h5>
+            <h6>{props.payload[13].studentPercent}</h6>
+        </div>
+        <div className='mainBlock'>
+            <img src={props.payload[14].studentPhoto} alt="" />
+            <h1>{props.payload[14].studentID}</h1>
+            <h2>{props.payload[14].student}</h2>
+            <h3>{props.payload[14].stream}</h3>
+            <h4>{props.payload[14].mockRating}</h4>
+            <h5>{props.payload[14].course}</h5>
+            <h6>{props.payload[14].studentPercent}</h6>
+        </div>
+        <div className='mainBlock'>
+            <img src={props.payload[15].studentPhoto} alt="" />
+            <h1>{props.payload[15].studentID}</h1>
+            <h2>{props.payload[15].student}</h2>
+            <h3>{props.payload[15].stream}</h3>
+            <h4>{props.payload[15].mockRating}</h4>
+            <h5>{props.payload[15].course}</h5>
+            <h6>{props.payload[15].studentPercent}</h6>
+        </div>
+        <div className='mainBlock'>
+            <img src={props.payload[16].studentPhoto} alt="" />
+            <h1>{props.payload[16].studentID}</h1>
+            <h2>{props.payload[16].student}</h2>
+            <h3>{props.payload[16].stream}</h3>
+            <h4>{props.payload[16].mockRating}</h4>
+            <h5>{props.payload[16].course}</h5>
+            <h6>{props.payload[16].studentPercent}</h6>
+        </div>
+        <div className='mainBlock'>
+            <img src={props.payload[17].studentPhoto} alt="" />
+            <h1>{props.payload[17].studentID}</h1>
+            <h2>{props.payload[17].student}</h2>
+            <h3>{props.payload[17].stream}</h3>
+            <h4>{props.payload[17].mockRating}</h4>
+            <h5>{props.payload[17].course}</h5>
+            <h6>{props.payload[17].studentPercent}</h6>
+        </div>
+        <div className='mainBlock'>
+            <img src={props.payload[18].studentPhoto} alt="" />
+            <h1>{props.payload[18].studentID}</h1>
+            <h2>{props.payload[18].student}</h2>
+            <h3>{props.payload[18].stream}</h3>
+            <h4>{props.payload[18].mockRating}</h4>
+            <h5>{props.payload[18].course}</h5>
+            <h6>{props.payload[18].studentPercent}</h6>
+        </div>
+        <div className='mainBlock'>
+            <img src={props.payload[19].studentPhoto} alt="" />
+            <h1>{props.payload[19].studentID}</h1>
+            <h2>{props.payload[19].student}</h2>
+            <h3>{props.payload[19].stream}</h3>
+            <h4>{props.payload[19].mockRating}</h4>
+            <h5>{props.payload[19].course}</h5>
+            <h6>{props.payload[19].studentPercent}</h6>
+        </div>
+
+    </div>
+  )
+}
+
+export default Student
